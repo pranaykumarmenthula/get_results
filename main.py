872fa7fd4f1c12d668b9ff9780b81daefcb6cbf4
pranaykumarmenthula <110916035+pranaykumarmenthula@users.noreply.github.com>
2023-09-10 14:01:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 
 
-imdata = ['sem4aiml.xlsx','sem4ce.xlsx','sem4cse.xlsx','sem4ds.xlsx' ,
-          'sem4ece.xlsx' ,'sem4eee.xlsx' ,'sem4it.xlsx' ,'sem4mech.xlsx']
+imdata = ['sem4cse.xlsx','sem4aiml.xlsx','sem4ds.xlsx','sem4cs.xlsx' ,
+          'sem4csit.xlsx' ,'sem4ce.xlsx' ,'sem4eee.xlsx' ,'sem4mech.xlsx', 
+          'sem4ece.xlsx' , 'sem4it.xlsx' , 'sem4aero.xlsx']
 
 idata=[]
 ihs=[]
@@ -25,10 +26,10 @@ data=[x for x in idata]
 headings_sheet=[x for x in ihs]
 
 
-branches = [ "CSE(AIML)",
-            "Civil Engineering" ,"Computer Science Engineering",
-            "CSE(DS)" ,"Electronics and Communication Engineering" , 
-            "EEE","Information Technology" , "Mechanical Engineering"]
+branches = [ "Computer Science Engineering","CSE(AIML)","CSE(DS)" ,"CS" ,"CSIT" ,
+            "Civil Engineering" ,"EEE","Mechanical Engineering" ,
+            "Electronics and Communication Engineering" , 
+            "Information Technology" , "AERO"]
 semesters = ["1st Semester", "2nd Semester", "3rd Semester", "4th Semester", "5th Semester", "6th Semester", "7th Semester", "8th Semester"]
 exam_types = ["CIE-I","CIE-II" , "Final Exam"]
 
@@ -48,11 +49,11 @@ res_d=[]
 
 
 
-for i in range(len(data)):
-    for y in range(len(semesters)):
-        if selected_semester == semesters[y]:
+for y in range(len(semesters)):
+    if selected_semester == semesters[y]:
+        for i in range(len(data)):
             for j in range(len(branches)):
-                if selected_branch == branches[j] :
+                if (selected_branch == branches[j] and selected_semester == semesters[y]):
 
                     filtered_data = data[j][((data[j]['Branch'] == selected_branch) & (data[j]['Exam Type'] == selected_exam_type) & (data[j]['Rollno'] == roll_number)) ]
  
@@ -62,9 +63,13 @@ for i in range(len(data)):
     
 
 
-if (len(res_d)>0):
-    st.write("Student Marks for Selected Branch, Semester, and Roll Number:")
-    st.write(res_d[0])
+if ((len(res_d)>0)):
+    res_sem=res_d[0]['Semester']
+    if str(selected_semester) in str(res_sem) :
+        st.write("Student Marks for Selected Branch, Semester, and Roll Number:")
+        st.write(res_d[0])
+    else:
+        st.write("No data found for the selected options.")
 else:
     st.write("No data found for the selected options.")
 
@@ -73,6 +78,7 @@ else:
 
 
 st.write(f"You selected: Branch - {selected_branch}, Semester - {selected_semester}, Exam Type - {selected_exam_type}")
+st.caption('Currently works for only :blue[4th Semester] ')
 
 hide_st_style = """
             <style>
