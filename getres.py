f5d2096d4cb2ced0d2eb86d6fe5292a59b65c322
pranaykumarmenthula <110916035+pranaykumarmenthula@users.noreply.github.com>
2023-09-23 17:14:56 +0530

    Update getres.py

--- getres.py
+++ getres.py
@@ -27,70 +27,79 @@ custom_css = """
 
 st.markdown(custom_css, unsafe_allow_html=True)
 
+selected_opt = option_menu(
+    menu_title=None,
+    options=["GetRes Anonymous" , "GetRes Credentials" , "Get Attendance"],
+    icons=["person-fill-lock" , "person-check-fill" , "file-bar-graph-fill"] ,
+    menu_icon="cast" ,
+    default_index=0 ,
+    orientation="horizontal"
+)
 
-imdata = ['sem4cse.xlsx','sem4aiml.xlsx','sem4ds.xlsx' ,'sem4cs.xlsx','sem4csit.xlsx' ,'sem4ce.xlsx',
+if selected_opt == "GetRes Anonymous" :
+            imdata = ['sem4cse.xlsx','sem4aiml.xlsx','sem4ds.xlsx' ,'sem4cs.xlsx','sem4csit.xlsx' ,'sem4ce.xlsx',
           'sem4eee.xlsx' ,'sem4mech.xlsx' ,'sem4ece.xlsx' ,'sem4it.xlsx' ,'sem4aero.xlsx']
 
-idata=[]
-ihs=[]
+            idata=[]
+            ihs=[]
 
-for i in range(len(imdata)):
-    def load_data():
-        df = pd.read_excel( imdata[i] , engine='openpyxl')
-        return df
-    data = load_data()
-    hs = data.columns
-    idata.append(data)
-    ihs.append(hs)
-
-
-data=[x for x in idata]
-headings_sheet=[x for x in ihs]
+            for i in range(len(imdata)):
+                def load_data():
+                    df = pd.read_excel( imdata[i] , engine='openpyxl')
+                    return df
+                data = load_data()
+                hs = data.columns
+                idata.append(data)
+                ihs.append(hs)
 
 
-branches = [ "Computer Science Engineering","CSE(AIML)","CSE(DS)" ,"CSE(CS)" ,"CSIT" ,
-            "Civil Engineering" ,"EEE","Mechanical Engineering" ,
-            "Electronics and Communication Engineering" , 
-            "Information Technology" , "AERO"]
-semesters = ["1st Semester", "2nd Semester", "3rd Semester", "4th Semester", "5th Semester", "6th Semester", "7th Semester", "8th Semester"]
-exam_types = ["CIE-I","CIE-II" , "Final Exam"]
+            data=[x for x in idata]
+            headings_sheet=[x for x in ihs]
 
 
-st.title("Get Results")
+            branches = [ "Computer Science Engineering","CSE(AIML)","CSE(DS)" ,"CSE(CS)" ,"CSIT" ,
+                        "Civil Engineering" ,"EEE","Mechanical Engineering" ,
+                        "Electronics and Communication Engineering" , 
+                        "Information Technology" , "AERO"]
+            semesters = ["1st Semester", "2nd Semester", "3rd Semester", "4th Semester", "5th Semester", "6th Semester", "7th Semester", "8th Semester"]
+            exam_types = ["CIE-I","CIE-II" , "Final Exam"]
 
-selected_branch = st.selectbox("Select Branch", branches)
-selected_semester = st.selectbox("Select Semester", semesters)
-selected_exam_type = st.selectbox("Select Exam Type", exam_types)
-roll_number = st.text_input("Enter Roll Number(In Uppercase!)")
 
-res_d=[]
+            st.title("Get Results")
 
-try:
-            for y in range(len(semesters)):
-                        if selected_semester == semesters[y]:
-                                    for i in range(len(data)):
-                                                for j in range(len(branches)):
-                                                            if (selected_branch == branches[j] and selected_semester == semesters[y]):
-                                                                        filtered_data = data[j][((data[j]['Branch'] == selected_branch) & (data[j]['Exam Type'] == selected_exam_type) & (data[j]['Rollno'] == roll_number.upper())) ]
-                                                                        if not filtered_data.empty:
-                                                                                    res_d.append(filtered_data[[str(k) for k in headings_sheet[j]]])
-                                                                                    break
+            selected_branch = st.selectbox("Select Branch", branches)
+            selected_semester = st.selectbox("Select Semester", semesters)
+            selected_exam_type = st.selectbox("Select Exam Type", exam_types)
+            roll_number = st.text_input("Enter Roll Number(In Uppercase!)")
+
+            res_d=[]
+
+            try:
+                        for y in range(len(semesters)):
+                                    if selected_semester == semesters[y]:
+                                                for i in range(len(data)):
+                                                            for j in range(len(branches)):
+                                                                        if (selected_branch == branches[j] and selected_semester == semesters[y]):
+                                                                                    filtered_data = data[j][((data[j]['Branch'] == selected_branch) & (data[j]['Exam Type'] == selected_exam_type) & (data[j]['Rollno'] == roll_number.upper())) ]
+                                                                                    if not filtered_data.empty:
+                                                                                                res_d.append(filtered_data[[str(k) for k in headings_sheet[j]]])
+                                                                                                break
     
-            if ((len(res_d)>0)):
-                        s1=str(selected_semester)
-                        s2=str(res_d[0])
-                        if re.search(s1, s2):
-                                    st.write("Student Marks for Selected Branch, Semester, and Roll Number:")
-                                    st.write(res_d[0])
+                        if ((len(res_d)>0)):
+                                    s1=str(selected_semester)
+                                    s2=str(res_d[0])
+                                    if re.search(s1, s2):
+                                                st.write("Student Marks for Selected Branch, Semester, and Roll Number:")
+                                                st.write(res_d[0])
+                                    else:
+                                                st.write("No data found for the selected options.")
                         else:
                                     st.write("No data found for the selected options.")
-            else:
-                        st.write("No data found for the selected options.")
-            st.write(f"You selected: Branch - {selected_branch}, Semester - {selected_semester}, Exam Type - {selected_exam_type}")
-            st.caption('Currently works for only :blue[4th Semester] ')
+                        st.write(f"You selected: Branch - {selected_branch}, Semester - {selected_semester}, Exam Type - {selected_exam_type}")
+                        st.caption('Currently works for only :blue[4th Semester] ')
 
 
-except Exception as e:
-        st.error(f"An error occurred : {str(e)}")
-        st.write("Working on Issues...")
+            except Exception as e:
+                    st.error(f"An error occurred : {str(e)}")
+                    st.write("Working on Issues...")
 
