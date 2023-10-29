e85621fd0d01b41ed297373c487ae3f9900e4d1e
pranaykumarmenthula <110916035+pranaykumarmenthula@users.noreply.github.com>
2023-10-29 16:31:25 +0530

    Update getres.py

--- getres.py
+++ getres.py
@@ -303,20 +303,10 @@ if selected_opt == "Get Attendance" :
                 "authority": "samvidha.iare.ac.in",
                 "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
                 "accept-language": "en-US,en;q=0.8",
-                "cache-control": "max-age=0",
                 "content-type": "application/x-www-form-urlencoded",
-                "cookie": "PHPSESSID=na6qqne9vmqpsu5fhf7lulmg75",
+                "cookie": "PHPSESSID=l53q8ottmsgcjnu41c9la4inv0",
                 "origin": "https://samvidha.iare.ac.in",
                 "referer": "https://samvidha.iare.ac.in/home?action=stud_att_hod",
-                "sec-ch-ua": "^\^Chromium^^;v=^\^118^^, ^\^Brave^^;v=^\^118^^, ^\^Not=A?Brand^^;v=^\^99^^",
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "^\^Windows^^",
-                "sec-fetch-dest": "document",
-                "sec-fetch-mode": "navigate",
-                "sec-fetch-site": "same-origin",
-                "sec-fetch-user": "?1",
-                "sec-gpc": "1",
-                "upgrade-insecure-requests": "1",
                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
             }
             response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
