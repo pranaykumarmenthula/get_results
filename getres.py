ffa2490a4cbca109ab2f278873bfe07d049e3f2e
pranaykumarmenthula <110916035+pranaykumarmenthula@users.noreply.github.com>
2023-12-26 20:30:31 +0530

    Update getres.py

--- getres.py
+++ getres.py
@@ -53,7 +53,6 @@ custom_css = """
 st.markdown(custom_css, unsafe_allow_html=True)
 
 os.system("playwright install")
-os.system("playwright install-deps")
 custom_js = """
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1009909662863685"
      crossorigin="anonymous">
