edad423617612c211e8a313d783cba0004134722
pranaykumarmenthula <110916035+pranaykumarmenthula@users.noreply.github.com>
2024-01-05 22:14:49 +0530

    Update getres.py

--- getres.py
+++ getres.py
@@ -6,7 +6,13 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import plotly.express as px
-import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
 
 
 st.set_page_config(
@@ -53,7 +59,7 @@ custom_css = """
 st.markdown(custom_css, unsafe_allow_html=True)
 
 
-os.system("playwright install --with-deps chromium")
+
 custom_js = """
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1009909662863685"
      crossorigin="anonymous">
@@ -302,103 +308,83 @@ if selected_opt == "Get Attendance" :
                         try:
                                     wait_message = st.empty()
                                     wait_message.text("Please wait...")
-                                    import asyncio
-                                    from playwright.async_api import async_playwright
-                                    async def get_session_id():
-                                                async with async_playwright() as p:
-                                                            browser = await p.chromium.launch()
-                                                            context = await browser.new_context()
-                                                            page = await context.new_page()
-                                                            await page.goto('https://samvidha.iare.ac.in/home')
-                                                            await page.fill('input[name="txt_uname"]', 'aimlhod')
-                                                            await page.fill('input[name="txt_pwd"]', 'aiml@95')
-                                                            await page.click('button[type="submit"]')
-                                                            await page.wait_for_load_state("load")
-                                                            await page.wait_for_timeout(1000)
-                                                            await page.goto('https://samvidha.iare.ac.in/home?action=stud_att_hod')
-                                                            await page.wait_for_timeout(1000)
-                                                            await page.fill('input[name="rollno"]', "21951A")
-                                                            await page.wait_for_timeout(1000)
-                                                            await page.click('input[name="ok_roll"]')
-                                                            await page.wait_for_timeout(1000)
-                                                            cookies = await context.cookies()
-                                                            session_id = next((cookie['value'] for cookie in cookies if cookie['name'] == 'PHPSESSID'), None)
-                                                            return session_id
-                                    if __name__ == '__main__':
-                                                loop = asyncio.new_event_loop()
-                                                asyncio.set_event_loop(loop)
-                                                session_id = loop.run_until_complete(get_session_id())
-                                                url = "https://samvidha.iare.ac.in/home"
-                                                querystring = {"action":"stud_att_hod"}
-                                                payload = "rollno="+str(rollno)+"&ok_roll=GET%2BATTENDANCE"
-                                                headers = {
-                                                    "authority": "samvidha.iare.ac.in",
-                                                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-                                                    "accept-language": "en-US,en;q=0.8",
-                                                    "content-type": "application/x-www-form-urlencoded",
-                                                    "cookie": "PHPSESSID="+str(session_id),
-                                                    "origin": "https://samvidha.iare.ac.in",
-                                                    "referer": "https://samvidha.iare.ac.in/home?action=stud_att_hod",
-                                                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
-                                                }
-                                                response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+                                    chrome_options = Options()
+                                    chrome_options.add_argument('--headless')
+                                    chrome_options.add_argument("--no-sandbox")
+                                    chrome_options.add_argument("--disable-dev-shm-usage")
+                                    driver = webdriver.Chrome(options=chrome_options)
+                                    driver.get('https://samvidha.iare.ac.in/home')
+                                    username_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'txt_uname')))
+                                    password_input = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'txt_pwd')))
+                                    login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Sign In")]')))
+                                    username_input.send_keys('aimlhod')
+                                    password_input.send_keys('aiml@95')
+                                    login_button.click()
+                                    time.sleep(4)
+                                    driver.get('https://samvidha.iare.ac.in/home?action=stud_att_hod')
+                                    search_bar = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'rollno')))
+                                    search_bar.send_keys(rollno)
+                                    response = search_bar.send_keys(Keys.RETURN)
+                                    time.sleep(4)
+                                    page_source = driver.page_source
+                                    soup = BeautifulSoup(page_source, 'html.parser')
+                                    table = soup.find('table', class_='table table-striped table-bordered table-hover table-head-fixed responsive ')
 
-                                                soup = BeautifulSoup(response.text, 'html.parser')
-                                                table = soup.find('table', class_='table table-striped table-bordered table-hover table-head-fixed responsive ')
+                                    thead = soup.find('thead')
+                                    headings = [th.text for th in thead.find_all('th')]
 
-                                                thead = soup.find('thead')
-                                                headings = [th.text for th in thead.find_all('th')]
-
-                                                tbody = soup.find('tbody')
-                                                table_data = []
-                                                for row in tbody.find_all('tr'):
-                                                            row_data = [td.text for td in row.find_all('td')]
-                                                            table_data.append(row_data)
+                                    tbod = soup.find_all('tbody')
+                                    tbody=tbod[2]
+                                    table_data = []
+                                    rows=tbody.find_all('tr')
+                                    for row in rows:
+                                                row_data = [td.text for td in row.find_all('td')]
+                                                table_data.append(row_data)
  
-                                                df = pd.DataFrame(table_data, columns=headings)
+                                    df = pd.DataFrame(table_data, columns=headings)
             
 
-                                                lst_cond = [x for x in df['Conducted']]
-                                                for i in range(len(lst_cond)):
-                                                            if lst_cond[i]=="":
-                                                                        lst_cond[i]='0'
+                                    lst_cond = [x for x in df['Conducted']]
+                                    for i in range(len(lst_cond)):
+                                                if lst_cond[i]=="":
+                                                            lst_cond[i]='0'
+                                                else:
+                                                            pass
+                                    lst_att = [x for x in df['Attended']]
+                                    for i in range(len(lst_cond)):
+                                                if lst_att[i]=="":
+                                                            lst_att[i]='0'
+                                                else:
+                                                            pass
+            
+                                    lst_final = []
+                                    att_numbers = [int(num) for num in lst_att]
+                                    cond_numbers = [int(num) for num in lst_cond]
+                                    for i in range(len(lst_att)):
+                                                x = cond_numbers[i]
+                                                y= att_numbers[i]
+                                                if y<(x*0.75):
+                                                            z=(x-y)*4
+                                                            fin = z - x
+                                                            lst_final.append(fin)
+                                                else:
+                                                            lst_final.append(0)
+            
+                                    lst_sub = [x for x in df['Course Name']]
+                                    try:
+                                                for i in range(len(lst_final)):
+                                                            if lst_final[i] != 0:
+                                                                        st.write(lst_sub[i] ,": :red[{} Classes]".format(int(lst_final[i])) )
                                                             else:
                                                                         pass
-                                                lst_att = [x for x in df['Attended']]
-                                                for i in range(len(lst_cond)):
-                                                            if lst_att[i]=="":
-                                                                        lst_att[i]='0'
-                                                            else:
-                                                                        pass
-            
-                                                lst_final = []
-                                                att_numbers = [int(num) for num in lst_att]
-                                                cond_numbers = [int(num) for num in lst_cond]
-                                                for i in range(len(lst_att)):
-                                                            x = cond_numbers[i]
-                                                            y= att_numbers[i]
-                                                            if y<(x*0.75):
-                                                                        z=(x-y)*4
-                                                                        fin = z - x
-                                                                        lst_final.append(fin)
-                                                            else:
-                                                                        lst_final.append(0)
-            
-                                                lst_sub = [x for x in df['Course Name']]
-                                                try:
-                                                            for i in range(len(lst_final)):
-                                                                        if lst_final[i] != 0:
-                                                                                    st.write(lst_sub[i] ,": :red[{} Classes]".format(int(lst_final[i])) )
-                                                                        else:
-                                                                                    pass
-                                                            wait_message.text("")
-                                                            st.write(df)
-                                                except Exception as e:
-                                                            st.success(":green[Satisfactory]")
+                                                wait_message.text("")
+                                                st.write(df)
+                                    except Exception as e:
+                                                st.success(":green[Satisfactory]")
             
 
                         except Exception as e:
-                                    st.write(e)    
+                                    st.error("Something went wrong... Please try again!!")    
 
 
 ad_code = """
